10af697512dbd0c539089fd313063399576e41df
SASBoom <balrajshah@live.com>
2020-03-06 14:23:12 -0500

    Initial Commit

---
+++ knexfile.js
@@ -0,0 +1,28 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      port: 5432,
+      user: 'postgres',
+      password: 'password',
+      database: 'mindmatch_platform',
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+};
