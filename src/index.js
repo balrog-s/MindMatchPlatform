84d78f9b8aab3f103d6b1d7a45f44d91517bb619
SASBoom <balrajshah@live.com>
2020-03-06 14:27:07 -0500

    Surface mutations and queries for graphql endpoint

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import graphqlHTTP from "express-graphql";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import {
+  GraphQLObjectType,
+  GraphQLSchema,
+} from "graphql";
+
+//MUTATIONS
+import createUser from './users/mutations/create-user';
+import updateUser from './users/mutations/update-user';
+import loginUser from './users/mutations/login-user';
+
+//QUERIES
+import getUsers from './users/queries/getUsers';
+import getUser from './users/queries/getUser';
+
+//AUTH
+import { auth } from './authz/authToken';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", function (req, res) {
+  res.send("Welcome to my API: Version 1.0.0");
+});
+
+app.use(bodyParser.json());
+app.use(auth);
+
+app.use("/graphql", cors(), graphqlHTTP(req => ({
+  context: { user: req.user, isAuthenticated: req.isAuthenticated },
+  schema: new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: "RootQueryType",
+      fields: {
+        users: getUsers,
+        user: getUser
+      }
+    }),
+    mutation: new GraphQLObjectType({
+      name: "RootMutationType",
+      fields: () => ({
+        CreateUser: createUser,
+        UpdateUser: updateUser,
+        LoginUser: loginUser
+      })
+    })
+  }),
+  graphiql: true
+})));
+
+app.listen(port, () => {
+  console.log(`LISTENING ON ${port}`);
+});
