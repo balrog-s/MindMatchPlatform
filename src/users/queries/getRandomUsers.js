5b5f55e025c60681c4d82b49a04c20a77c71d4f9
sasboom <balrajshah@live.com>
2020-04-14 01:38:18 -0400

    Update getRandomUsers to not show users already requested

--- src/users/queries/getRandomUsers.js
+++ src/users/queries/getRandomUsers.js
@@ -19,16 +19,19 @@ const getRandomUsers = (ctx) => {
   if (!ctx.isAuthenticated) {
     throw new Error(`User is not authenticated`);
   }
-  return pg('core.users')
+  return pg('core.users as users')
     .select(userProperties)
-    .whereNot({
-      id: ctx.user.id
-    })
+    .where(
+      pg.raw(`
+        users.id not in (select requested_user_id from core.users_matches as um where um.initiator_user_id = ?)
+        and users.id != ?
+      `, [ctx.user.id, ctx.user.id])
+    )
     .orderBy(pg.raw('random()'))
     .limit(6)
     .then(humps.camelizeKeys)
     .then(users => ({ error: false, payload: users }))
-    .catch(err => ({ error: true, payload: err }));
+    .catch(err => ({ error: true, payload: err }));;
 };
 
 module.exports = {
