698752850199f0393d6e35cd84c8eeb93873f46e
sasboom <balrajshah@live.com>
2020-06-07 23:56:02 -0400

    Clean up index file

--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
-const { ApolloServer } = require('apollo-server-express');
-
-import {
-  GraphQLObjectType,
-  GraphQLSchema,
-} from "graphql";
-
-//USER MUTATIONS
-import createUser from './users/mutations/create-user';
-import updateUser from './users/mutations/update-user';
-import loginUser from './users/mutations/login-user';
-import userCreatedSubscription from './users/subscriptions/user-created';
-
-//USER QUERIES
-import getUsers from './users/queries/getUsers';
-import getUser from './users/queries/getUser';
-import getRandomUsers from './users/queries/getRandomUsers';
-
-//MATCH MUTATIONS
-import matchRequest from './matches/mutations/match-request';
-import matchUpdate from './matches/mutations/match-update';
-
-//MATCH QUERIES
-import getMatches from './matches/queries/getMatches';
-
-//PROFILE MUTATIONS
-import createProfile from './profiles/mutations/create-profile';
-import updateProfile from './profiles/mutations/update-profile';
-
-//PROFILE QUERIES
-import getProfile from './profiles/queries/getProfile';
-
+import { ApolloServer } from 'apollo-server-express';
 //AUTH
 import { auth } from './authz/authToken';
 
@@ -41,7 +9,7 @@ import typeDefs from './typeDefs';
 import http from 'http';
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(auth);
@@ -49,7 +17,7 @@ app.use(auth);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({req, res}) => {
+  context: ({req}) => {
     return { user: req.user || null, isAuthenticated: req.isAuthenticated }
   },
   subscriptions: {
